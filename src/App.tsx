90f479604a0a6605e733862a4a605ad8c2216768
Riyansu <kenKEN.DEM@gmail.com>
2024-01-16 18:20:26 +0900

    SolidJS Practice

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Component, createSignal, createEffect } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  const [name,setName] = createSignal("")
+  createEffect(()=>{
+    console.log(name());
+  })
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a><br />
+        Hello, {name()}
+        <br />
+        <input type="text" value={name()} onInput={(e)=>{setName(e.target.value)}}/>
+      </header>
+    </div>
+  );
+};
+
+export default App;
